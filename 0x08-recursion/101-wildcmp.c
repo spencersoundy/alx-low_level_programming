d296d933e3d8a705ae39fcecb5ea8f9be3ecc1e0
spencersoundy <spencersoundy@gmail.com>
2023-08-02 09:19:28 -0700

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,31 +1,33 @@
 #include "main.h"
 
 /**
- * wildcmp - compare strings
- * @s1: pointer to string params
- * @s2: pointer to string params
- *
- * Return: 0
+ * str_checker - chech if two strings are identical
+ * @s1: string 1 base address
+ * @s2: string 2 base address
+ * @i: left index
+ * @j: special index
+ * Return: 1 if s is palindrome 0 otherwise
  */
-
+int str_checker(char *s1, char *s2, int i, int j)
+{
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
+}
+/**
+ * wildcmp - check if strings could be considered indentical
+ * @s1: base address for string
+ * @s2: base address for string
+ *
+ * Return: 1 if considered identical
+ */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0')
-	{
-		if (*s2 != '\0' && *s2 == '*')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		return (*s2 === '\0');
-	}
-
-	if (*s2 == '*')
-	{
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	}
-	else if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	return (0);
+	return (str_checker(s1, s2, 0, 0));
 }
