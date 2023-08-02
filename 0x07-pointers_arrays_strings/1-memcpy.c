49310f0d72c1733477e3fecc6911bf831554e264
spencersoundy <spencersoundy@gmail.com>
2023-08-02 03:14:15 -0700

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,12 +12,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	insigned int i = 0;
+	insigned int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 
 	return (dest);
